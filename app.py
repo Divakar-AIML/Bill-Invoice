767526ce945fc811787fb136dc6235fcd9612f8d
Hygeia-Nexus <your.email@example.com>
2025-06-28 00:28:49 +0530

    updated app.py

--- app.py
+++ app.py
@@ -52,11 +52,13 @@ if uploaded_files:
                 all_skipped.append(line)
             for mismatch in mismatched:
                 all_mismatched.append(mismatch)
-    if "Ship To" in df.columns:
-        df = df.drop(columns=["Ship To"])
+    
 
     final_df = pd.concat(all_df, ignore_index=True)
     final_summary = pd.concat(all_summary, ignore_index=True) if all_summary else None
+    
+    if "Ship To" in df.columns:
+        df = df.drop(columns=["Ship To"])
 
     st.success("✅ Processing complete!")
 
